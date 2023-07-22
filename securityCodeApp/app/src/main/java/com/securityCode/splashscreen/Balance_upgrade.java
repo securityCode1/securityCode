7c4172ce519e6375cb1c0fbd6af4c4f698665f02
moaizahmed1 <139141288+moaizahmed1@users.noreply.github.com>
2023-07-22 16:38:50 +0500

    balance update

--- securityCodeApp/app/src/main/java/com/securityCode/splashscreen/Balance_upgrade.java
+++ securityCodeApp/app/src/main/java/com/securityCode/splashscreen/Balance_upgrade.java
@@ -21,40 +21,13 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Balance_upgrade extends AppCompatActivity {
-    Button Update_bal,cancel_btn;
-    TextInputEditText New_amount;
-    private TextView currentTv;
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-    String Uid;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balance_upgrade);
 
-        Update_bal=findViewById(R.id.bal_update);
-        New_amount=findViewById(R.id.New_amount);
-        Uid= FirebaseAuth.getInstance().getUid();
-        currentTv=findViewById(R.id.TvCurrent);
-        cancel_btn=findViewById(R.id.btn_cancel);
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat simpleD = new SimpleDateFormat("MMMyyyy");
-        String monthId = simpleD.format(cal.getTime());
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("users/"+Uid+"/expenseList/"+monthId+"/New Balance/");
 
-        Update_bal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Calendar calendar = Calendar.getInstance();
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddhhmmssa");
-                String expId = simpleDateFormat.format(calendar.getTime());
-                currentTv.setText(expId);
-                Toast.makeText(Balance_upgrade.this, ": ", Toast.LENGTH_SHORT).show();
-//                myRef.child(expId).child(New_amount.getText().toString()).setValue(New_amount.getText().toString());
-            }
-        });
     }
 }
--- securityCodeApp/app/src/main/java/com/securityCode/splashscreen/Expense.java
+++ securityCodeApp/app/src/main/java/com/securityCode/splashscreen/Expense.java
@@ -30,7 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Expense extends AppCompatActivity {
 
-    Button btn_expense,btn_confirm,Update_bal,cancel_btn;
+    Button btn_expense,btn_confirm,Update_bal,cancel_btn,btn_cancel;
 
     TextInputEditText New_amount;
     FirebaseDatabase database;
@@ -63,13 +63,7 @@ public class Expense extends AppCompatActivity {
         New_amount=dialog_add_balance.findViewById(R.id.New_amount);
         currentTv=dialog_add_balance.findViewById(R.id.TvCurrent);
         cancel_btn=dialog_add_balance.findViewById(R.id.btn_cancel);
-       
 
-
-        Update_bal=findViewById(R.id.bal_update);
-        New_amount=findViewById(R.id.New_amount);
-        currentTv=findViewById(R.id.TvCurrent);
-        cancel_btn=findViewById(R.id.btn_cancel);
         new_balance=findViewById(R.id.add_newBal);
         dialog_add_expense = new Dialog(Expense.this);
         dialog_add_expense.setContentView(R.layout.add_expense_dialog);
@@ -77,7 +71,7 @@ public class Expense extends AppCompatActivity {
         expenseType=dialog_add_expense.findViewById(R.id.expense_txt);
         btn_confirm=dialog_add_expense.findViewById(R.id.btn_confirm_expense);
         expenseAmount=dialog_add_expense.findViewById(R.id.total_amount);
-
+        btn_cancel=dialog_add_expense.findViewById(R.id.btn_cancel);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, items);
 
@@ -89,14 +83,14 @@ public class Expense extends AppCompatActivity {
         SimpleDateFormat simpleD = new SimpleDateFormat("MMMyyyy");
         String monthId = simpleD.format(cal.getTime());
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("users/"+Uid+"/expenseList/"+monthId);
+        myRef = database.getReference("users/"+Uid+"/expenseList/"+monthId+"/Balance/");
 
         btn_confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Calendar calendar = Calendar.getInstance();
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddhhmmssa");
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dMMMyyyy");
                 String expId = simpleDateFormat.format(calendar.getTime());
                 currentTV.setText(expId);
                 Toast.makeText(Expense.this, ": "+expenseType.getText().toString(), Toast.LENGTH_SHORT).show();
@@ -118,7 +112,30 @@ public class Expense extends AppCompatActivity {
             }
 
         });
+        Update_bal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentTV.setText(monthId);
+                Toast.makeText(Expense.this, " BALANCE UPDATED", Toast.LENGTH_SHORT).show();
+               myRef.child(monthId).child(New_amount.getText().toString()).setValue(New_amount.getText().toString());
+            }
+        });
 
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    dialog_add_balance.cancel();
+
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog_add_expense.cancel();
+            }
+        });
             }
         }
 
